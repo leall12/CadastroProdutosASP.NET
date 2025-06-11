54b8616ee5ba6aa60bde05aaa4bd56a6d3257b64
leall12 <arturalbertinileal@gmail.com>
2025-06-11 07:56:32 -0300

    Criando Controllers

---
+++ CadastroProdutos/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroProdutos.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+    }
+}
