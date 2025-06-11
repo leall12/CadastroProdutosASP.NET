4ab9feafccbcadbabf71d0efe8ae0c655f8db7c3
leall12 <arturalbertinileal@gmail.com>
2025-06-11 07:51:50 -0300

    Fazendo os códigos do Repositorio

--- CadastroProdutos/Repositorio/ProdutoRepositorio.cs
+++ CadastroProdutos/Repositorio/ProdutoRepositorio.cs
@@ -1,8 +1,82 @@
-﻿using MySql.Data.MySqlClient;
-using System.Data;
+﻿using System.Data;
+using CadastroProdutos.Models;
+using MySql.Data.MySqlClient;
+
 namespace CadastroProdutos.Repositorio
 {
-    public class ProdutoRepositorio
+    // Define a classe UsuarioRepositorio, responsável por operações de acesso a dados para a entidade Usuario.
+    public class ProdutoRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+        public Produto ObterProduto(string IdProd)
+        {
+            // Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+                conexao.Open();
+                // Cria um novo comando SQL para selecionar todos os campos da tabela 'Usuario' onde o campo 'Email' corresponde ao parâmetro fornecido.
+                MySqlCommand cmd = new("SELECT * FROM Produtos WHERE IdProd = @IdProd", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', especificando o tipo como VarChar e utilizando o valor do parâmetro 'email'.
+                cmd.Parameters.Add("@IdProd", MySqlDbType.VarChar).Value = IdProd;
+
+                // Executa o comando SQL SELECT e obtém um leitor de dados (MySqlDataReader). O CommandBehavior.CloseConnection garante que a conexão
+                // será fechada automaticamente quando o leitor for fechado.
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    // Inicializa uma variável 'usuario' como null. Ela será preenchida se um usuário for encontrado.
+                    Produto produto = null;
+                    // Lê a próxima linha do resultado da consulta. Retorna true se houver uma linha e false caso contrário.
+                    if (dr.Read())
+                    {
+                        // Cria uma nova instância do objeto 'Usuario'.
+                        produto = new Produto
+                        {
+                            // Lê o valor da coluna "Id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'Id' do objeto 'usuario'.
+                            IdProd = Convert.ToInt32(dr["IdProd"]),
+                            // Lê o valor da coluna "Nome" da linha atual do resultado, converte para string e atribui à propriedade 'Nome' do objeto 'usuario'.
+                            Nome = dr["Nome"].ToString(),
+                            // Lê o valor da coluna "Email" da linha atual do resultado, converte para string e atribui à propriedade 'Email' do objeto 'usuario'.
+                            Descricao = dr["Descricao"].ToString(),
+                            // Lê o valor da coluna "Senha" da linha atual do resultado, converte para string e atribui à propriedade 'Senha' do objeto 'usuario'.
+                            Preco = Convert.ToDecimal( dr["Preco"]),
+                            Quantidade = Convert.ToInt32(dr["Quantidade"]),
+                        };
+                    }
+                    /* Retorna o objeto 'usuario'. Se nenhum usuário foi encontrado com o email fornecido, a variável 'usuario'
+                     permanecerá null e será retornado.*/
+                    return produto;
+                }
+            }
+        }
+        // Define um método público para adicionar um novo usuário ao banco de dados. Recebe um objeto 'Usuario' como parâmetro.
+        public void AdicionarProduto(Produto produto)
+        {
+            /* Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+             Isso garante que a conexão será fechada e descartada corretamente após o uso, mesmo em caso de erro.*/
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+
+                conexao.Open();
+                /* Cria um novo comando SQL para inserir dados na tabela 'Usuario'. Os valores para Nome, Email e Senha são passados como parâmetros
+                 (@Nome, @Email, @Senha) para evitar SQL Injection.*/
+
+                MySqlCommand cmd = new("INSERT INTO Produtos (Nome, Descricao, Preco, Quantidade) VALUES (@Nome,@Descricao,@Preco,@Quantidade)", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Nome', utilizando o valor da propriedade 'Nome' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Nome", produto.Nome);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', utilizando o valor da propriedade 'Email' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Descricao", produto.Descricao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Senha', utilizando o valor da propriedade 'Senha' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Preco", produto.Preco);
+                cmd.Parameters.AddWithValue("@Quantidade", produto.Quantidade);
+                // Executa o comando SQL INSERT no banco de dados. Retorna o número de linhas afetadas.
+                cmd.ExecuteNonQuery();
+                // Fecha a conexão com o banco de dados (embora o 'using' já faria isso, só garante o fechamento).
+                conexao.Close();
+            }
+        }
+
     }
 }
--- CadastroProdutos/Repositorio/UsuarioRepositorio.cs
+++ CadastroProdutos/Repositorio/UsuarioRepositorio.cs
@@ -1,8 +1,80 @@
-﻿using MySql.Data.MySqlClient;
-using System.Data;
+﻿using System.Data;
+using CadastroProdutos.Models;
+using MySql.Data.MySqlClient;
+
 namespace CadastroProdutos.Repositorio
 {
-    public class UsuarioRepositorio
+    // Define a classe UsuarioRepositorio, responsável por operações de acesso a dados para a entidade Usuario.
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+        public Usuario ObterUsuario(string email)
+        {
+            // Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+                conexao.Open();
+                // Cria um novo comando SQL para selecionar todos os campos da tabela 'Usuario' onde o campo 'Email' corresponde ao parâmetro fornecido.
+                MySqlCommand cmd = new("SELECT * FROM Usuarios WHERE Email = @email", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', especificando o tipo como VarChar e utilizando o valor do parâmetro 'email'.
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                // Executa o comando SQL SELECT e obtém um leitor de dados (MySqlDataReader). O CommandBehavior.CloseConnection garante que a conexão
+                // será fechada automaticamente quando o leitor for fechado.
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    // Inicializa uma variável 'usuario' como null. Ela será preenchida se um usuário for encontrado.
+                    Usuario usuario = null;
+                    // Lê a próxima linha do resultado da consulta. Retorna true se houver uma linha e false caso contrário.
+                    if (dr.Read())
+                    {
+                        // Cria uma nova instância do objeto 'Usuario'.
+                        usuario = new Usuario
+                        {
+                            // Lê o valor da coluna "Id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'Id' do objeto 'usuario'.
+                            IdUser = Convert.ToInt32(dr["IdUser"]),
+                            // Lê o valor da coluna "Nome" da linha atual do resultado, converte para string e atribui à propriedade 'Nome' do objeto 'usuario'.
+                            Nome = dr["Nome"].ToString(),
+                            // Lê o valor da coluna "Email" da linha atual do resultado, converte para string e atribui à propriedade 'Email' do objeto 'usuario'.
+                            Email = dr["Email"].ToString(),
+                            // Lê o valor da coluna "Senha" da linha atual do resultado, converte para string e atribui à propriedade 'Senha' do objeto 'usuario'.
+                            Senha = dr["Senha"].ToString()
+                        };
+                    }
+                    /* Retorna o objeto 'usuario'. Se nenhum usuário foi encontrado com o email fornecido, a variável 'usuario'
+                     permanecerá null e será retornado.*/
+                    return usuario;
+                }
+            }
+        }
+        // Define um método público para adicionar um novo usuário ao banco de dados. Recebe um objeto 'Usuario' como parâmetro.
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            /* Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+             Isso garante que a conexão será fechada e descartada corretamente após o uso, mesmo em caso de erro.*/
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+
+                conexao.Open();
+                /* Cria um novo comando SQL para inserir dados na tabela 'Usuario'. Os valores para Nome, Email e Senha são passados como parâmetros
+                 (@Nome, @Email, @Senha) para evitar SQL Injection.*/
+
+                MySqlCommand cmd = new("INSERT INTO Usuarios (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Nome', utilizando o valor da propriedade 'Nome' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', utilizando o valor da propriedade 'Email' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Senha', utilizando o valor da propriedade 'Senha' do objeto 'usuario'.
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                // Executa o comando SQL INSERT no banco de dados. Retorna o número de linhas afetadas.
+                cmd.ExecuteNonQuery();
+                // Fecha a conexão com o banco de dados (embora o 'using' já faria isso, só garante o fechamento).
+                conexao.Close();
+            }
+        }
+
     }
 }
